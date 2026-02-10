cde0205c12e5f53b9e78459fb911754e25e2cab3
venkatnivi062 <venkatnivi062@gmail.com>
2026-02-10 18:22:34 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,28 @@
+module "vpc" {
+  source     = "./modules/vpc"
+  cidr_block = var.cidr_block
+  env        = var.env
+}
+
+module "subnet" {
+  source      = "./modules/subnet"
+  vpc_id      = module.vpc.id
+  subnet_cidr = var.subnet_cidr
+  az          = "us-west-2a"
+  env         = var.env
+}
+
+module "sg" {
+  source = "./modules/security-group"
+  vpc_id = module.vpc.id
+  env    = var.env
+}
+
+module "ec2" {
+  source         = "./modules/ec2"
+  ami_id         = var.ami_id
+  instance_type  = var.instance_type
+  subnet_id      = module.subnet.id
+  sg_id          = module.sg.id
+  env            = var.env
+}
