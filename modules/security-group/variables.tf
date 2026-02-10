3c2945a8e0470d1b920a6dc5352ae3a72bd3b189
venkatnivi062 <venkatnivi062@gmail.com>
2026-02-10 18:30:15 +0530

    Create variables.tf

---
+++ modules/security-group/variables.tf
@@ -0,0 +1,9 @@
+variable "vpc_id" {
+  description = "VPC ID to attach the security group"
+  type        = string
+}
+
+variable "env" {
+  description = "Environment name"
+  type        = string
+}
