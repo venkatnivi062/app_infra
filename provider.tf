24b69c502e53d49644b5c35d222cb8ada5132c01
venkatnivi062 <venkatnivi062@gmail.com>
2026-02-10 14:43:46 +0530

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  backend "s3" {
+    bucket         = "my-terraform-state-bucket"
+    key            = "envs/dev/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
