7be086ad55643ff75f3a7dcee41840c4fd97562a
venkatnivi062 <venkatnivi062@gmail.com>
2026-02-13 14:49:58 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,8 +2,8 @@ output "ec2_public_ip" {
   value = module.ec2.ec2_public_ip
 }
 output "s3_bucket_id" {
-    value = module.s3_bucket.sample_bucket.id
+    value = module.aws_s3_bucket.sample_bucket.id
 }
 output "s3_bucket_arn" {
-    value = module.s3_bucket.sample_bucket.arn
+    value = module.aws_s3_bucket.sample_bucket.arn
 }
